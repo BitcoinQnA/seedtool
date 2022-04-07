b099025e10d9d1c0f7f55ed3ba64c6a8f63a3e0c
Super Phat Arrow <38541071+SuperPhatArrow@users.noreply.github.com>
2022-04-07 12:53:19 +0100

    remove console.log

--- src/www/js/dom.js
+++ src/www/js/dom.js
@@ -1299,11 +1299,8 @@ const setMnemonicFromEntropy = async () => {
   while (hex.length % 64 !== 0) {
     hex = `0${hex}`;
   }
-  console.log('hex :>> ', hex);
   const end = (mnemonicLength * 8) / 3;
-  console.log('end :>> ', end);
   const hexedBin = hex.slice(0, end);
-  console.log('hexedBin :>> ', hexedBin);
   // Convert entropy array to mnemonic
   const phrase = window.bip39.entropyToMnemonic(hexedBin);
   // Set the mnemonic in the UI
